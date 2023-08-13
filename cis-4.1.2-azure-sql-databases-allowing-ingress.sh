e98ae2b9d1293b33ee4693dcfeca4df08ab5803c
webpwnized <webpwnized@gmail.com>
2023-08-12 22:30:24 -0400

    1.0.2 Creating cis-4.1.2-azure-sql-databases-allowing-ingress.sh

--- cis-4.1.2-azure-sql-databases-allowing-ingress.sh
+++ cis-4.1.2-azure-sql-databases-allowing-ingress.sh
@@ -10,33 +10,30 @@ function output_header() {
 };
 
 function output_csv_header() {
-	echo "\"SUBSCRIPTION_NAME\",\"SUBSCRIPTION_STATE\",\"SUBSCRIPTION_ID\",\"RESOURCE_GROUP_NAME\",\"RESOURCE_GROUP_LOCATION\",\"RESOURCE_GROUP_APPLICATION_CODE\",\"RESOURCE_GROUP_DEPARTMENT_CHARGE_CODE\",\"RESOURCE_GROUP_PAR\",\"RESOURCE_GROUP_REQUESTOR_AD_ID\",\"RESOURCE_GROUP_REQUESTOR_EMPLOYEE_ID\"";
+	echo "\"SUBSCRIPTION_NAME\",\"RESOURCE_GROUP_NAME\",\"RESOURCE_GROUP_APPLICATION_CODE\",\"RESOURCE_GROUP_DEPARTMENT_CHARGE_CODE\",\"RESOURCE_GROUP_PAR\",\"RESOURCE_GROUP_REQUESTOR_AD_ID\",\"RESOURCE_GROUP_REQUESTOR_EMPLOYEE_ID\"";
 };
 
-function output_resource_group() {
+function output_sql_server() {
 	if [[ $RESOURCE_GROUP_NAME != "Visual Studio"* ]]; then
-		output_resource_group_helper;
+		output_sql_server_helper;
 	fi;
 };
 
-function output_resource_group_helper() {
+function output_sql_server_helper() {
 	if [[ $CSV == "True" ]]; then
-		output_resource_group_csv;
+		output_sql_server_csv;
 	else
-		output_resource_group_text;
+		output_sql_server_text;
 	fi;
 };
 
-function output_resource_group_csv() {
-	echo "\"$SUBSCRIPTION_NAME\",\"$SUBSCRIPTION_STATE\",\"$SUBSCRIPTION_ID\",\"$RESOURCE_GROUP_NAME\",\"$RESOURCE_GROUP_LOCATION\",\"$RESOURCE_GROUP_APPLICATION_CODE\",\"$RESOURCE_GROUP_DEPARTMENT_CHARGE_CODE\",\"$RESOURCE_GROUP_PAR\",\"$RESOURCE_GROUP_REQUESTOR_AD_ID\",\"$RESOURCE_GROUP_REQUESTOR_EMPLOYEE_ID\"";
+function output_sql_server_csv() {
+	echo "\"$SUBSCRIPTION_NAME\",\"$RESOURCE_GROUP_NAME\",\"$RESOURCE_GROUP_APPLICATION_CODE\",\"$RESOURCE_GROUP_DEPARTMENT_CHARGE_CODE\",\"$RESOURCE_GROUP_PAR\",\"$RESOURCE_GROUP_REQUESTOR_AD_ID\",\"$RESOURCE_GROUP_REQUESTOR_EMPLOYEE_ID\"";
 };
 
-function output_resource_group_text() {
+function output_sql_server_text() {
 	echo "Subscription Name: $SUBSCRIPTION_NAME";
-	echo "Subscription State: $SUBSCRIPTION_STATE";
-	echo "Subscription ID: $SUBSCRIPTION_ID";
 	echo "Resource Group Name: $RESOURCE_GROUP_NAME";
-	echo "Resource Group Location: $RESOURCE_GROUP_LOCATION";
 	echo "Resource Group Application Code: $RESOURCE_GROUP_APPLICATION_CODE";
 	echo "Resource Group Department Charge Code: $RESOURCE_GROUP_DEPARTMENT_CHARGE_CODE";
 	echo "Resource Group PAR: $RESOURCE_GROUP_PAR";
@@ -72,6 +69,11 @@ if [[ $SUBSCRIPTIONS != "[]" ]]; then
 			echo $RESOURCE_GROUPS | jq -rc '.[]' | while IFS='' read RESOURCE_GROUP;do
 
 				RESOURCE_GROUP_NAME=$(echo $RESOURCE_GROUP | jq -rc '.name');
+				RESOURCE_GROUP_APPLICATION_CODE=$(echo $RESOURCE_GROUP | jq -rc '.tags.applicationCode');
+				RESOURCE_GROUP_DEPARTMENT_CHARGE_CODE=$(echo $RESOURCE_GROUP | jq -rc '.tags.departmentChargeCode');
+				RESOURCE_GROUP_PAR=$(echo $RESOURCE_GROUP | jq -rc '.tags.par');
+				RESOURCE_GROUP_REQUESTOR_AD_ID=$(echo $RESOURCE_GROUP | jq -rc '.tags.requestorAdId');
+				RESOURCE_GROUP_REQUESTOR_EMPLOYEE_ID=$(echo $RESOURCE_GROUP | jq -rc '.tags.requestorEmployeeId');
 
 				declare SQL_SERVERS=$(get_azure_sql_servers $SUBSCRIPTION_NAME $RESOURCE_GROUP_NAME);
 
