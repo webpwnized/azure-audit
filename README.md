ab4273e40d54e95d4a3caaaaf4b7fa86c058723f
webpwnized <webpwnized@gmail.com>
2024-05-28 21:07:01 -0400

    1.0.18 Enhanced database scanner to try login

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # azure-audit
 
-The Azure Audit project automates many of the audits found in the Center for Internet Security (CIS) Azure Cloud Platform Foundation Benchmark. There is one script per benchmark. The script is named after the corresponding benchmark. The scripts can enumerate all projects in an organization and scan each project, or the user can pass in the project as a parameter.
+The Azure Audit project automates many of the audits found in the Center for Internet Security (CIS) Azure Cloud Platform Foundation Benchmark. There is one script per benchmark. The script is named after the corresponding benchmark. The scripts can enumerate all resource groups in an organization and scan each, or the user can pass in the subscription and resource group as a parameter.
 
-# Usage Instructions (Cloud Shell)
+## Usage Instructions (Cloud Shell)
 
-## Video Tutorial
+### Video Tutorial
 
 TODO
 
-# Usage Instructions (Local Terminal)
+## Usage Instructions (Local Terminal)
 
-## Video Tutorial
+### Video Tutorial
 
 TODO
 
@@ -18,13 +18,15 @@ TODO
 
 ### An operating system to install the needed software
 
-If you would like to use an Ubuntu virtual machine, [**install Ubuntu on VirtualBox**](https://www.youtube.com/watch?v=Cazzls2sZVk) or other hypervisor. Ubuntu runs better on VirtualBox if [**the Guest Additions are installed**](https://www.youtube.com/watch?v=8VCeFRwRmRU). If VirtualBox is not installed, [**install VirtualBox**](https://www.youtube.com/watch?v=61GhP8DsQMw).
+If you would like to use an Ubuntu virtual machine, you can [install Ubuntu on VirtualBox](https://www.youtube.com/watch?v=Cazzls2sZVk) or other hypervisor. Ubuntu runs better on VirtualBox if [the Guest Additions are installed](https://www.youtube.com/watch?v=8VCeFRwRmRU). If VirtualBox is not installed, you can [install VirtualBox](https://www.youtube.com/watch?v=61GhP8DsQMw).
 
 ### The Azure Cloud Platform (GPC) *az* client software
 
 TODO
 
 ### This project
+
+To clone this project, run the following command:
 
 `git clone https://github.com/webpwnized/azure-audit.git`
 
--- src/utility-list-subscriptions.sh
+++ src/utility-list-subscriptions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Debug: ./utility-list-subscriptions.sh --subscription 651b4cdc-83bc-466a-975d-df1a9c2be5b1
+
 # Source common constants and functions
 source ./common-constants.inc
 source ./functions.inc
@@ -46,26 +48,37 @@ function output_subscription_text() {
     echo $BLANK_LINE
 }
 
-# Parse subscription information
 function parse_subscription() {
-    SUBSCRIPTION_NAME=$(echo $SUBSCRIPTION | jq -rc '.displayName')
-    SUBSCRIPTION_STATE=$(echo $SUBSCRIPTION | jq -rc '.state')
-    SUBSCRIPTION_ID=$(echo $SUBSCRIPTION | jq -rc '.subscriptionId')
+    # Parse subscription information from JSON
+    SUBSCRIPTION_NAME=$(jq -rc '.displayName // empty' <<< "$SUBSCRIPTION")
+    SUBSCRIPTION_STATE=$(jq -rc '.state // empty' <<< "$SUBSCRIPTION")
+    SUBSCRIPTION_ID=$(jq -rc '.subscriptionId // empty' <<< "$SUBSCRIPTION")
 }
 
-# Process role assignments
-function process_role_assignments() {
-    declare -A unique_members  # Declare an associative array to store unique members
-    
-    while IFS='' read -r ROLE_ASSIGNMENT; do
-        PRINCIPLE_TYPE=$(echo "$ROLE_ASSIGNMENT" | jq -rc '.principalType')
+function get_role_assignments() {
+    # Get role assignments and store in a variable
+    ROLE_ASSIGNMENTS=$(get_subscription_role_assignments "$SUBSCRIPTION_NAME")
+}
 
+# Function to get and process role assignments
+function parse_role_assignments() {
+    
+    # Initialize associative array to store unique members
+    declare -A unique_members
+
+    # Iterate through each role assignment using a while loop
+    while IFS='' read -r ROLE_ASSIGNMENT; do
+        
+        output_debug_info "Role Assignment (JSON): $ROLE_ASSIGNMENT"
+
+        PRINCIPLE_TYPE=$(jq -rc '.principalType // empty' <<< "$ROLE_ASSIGNMENT")
+        ROLE_NAME=$(jq -rc '.roleDefinitionName // empty' <<< "$ROLE_ASSIGNMENT")
+        
         if [[ $PRINCIPLE_TYPE == "User" ]]; then
-            PRINCIPLE_NAME=$(echo "$ROLE_ASSIGNMENT" | jq -rc '.principalName')
+            PRINCIPLE_NAME=$(jq -rc '.principalName // empty' <<< "$ROLE_ASSIGNMENT")
             unique_members["$PRINCIPLE_NAME"]=1  # Store unique user in the associative array
         elif [[ $PRINCIPLE_TYPE == "Group" ]]; then
-            GROUP_NAME=$(echo "$ROLE_ASSIGNMENT" | jq -rc '.principalName')
-            ROLE=$(echo "$ROLE_ASSIGNMENT" | jq -rc '.roleDefinitionName')
+            GROUP_NAME=$(jq -rc '.principalName // empty' <<< "$ROLE_ASSIGNMENT")
             GROUP_MEMBERS=$(get_group_members_serialized "$GROUP_NAME")
 
             # Split group members and add unique members to the associative array
@@ -74,7 +87,7 @@ function process_role_assignments() {
                 unique_members["$member"]=1
             done
         fi   
-    done <<< "$(echo "$1" | jq -rc '.[]')"
+    done <<< "$(echo "$ROLE_ASSIGNMENTS" | jq -rc '.[]')"
 
     # Concatenate unique members from the associative array
     MEMBERS=""
@@ -83,7 +96,6 @@ function process_role_assignments() {
     done
 }
 
-
 # Source common menu
 source ./common-menu.inc
 
@@ -91,9 +103,7 @@ source ./common-menu.inc
 SUBSCRIPTIONS=$(get_subscriptions $p_SUBSCRIPTION_ID)
 
 # Debugging information
-if [[ $DEBUG == "True" ]]; then
-    echo "Subscriptions (JSON): $SUBSCRIPTIONS"
-fi
+output_debug_info "Subscriptions (JSON): $SUBSCRIPTIONS"
 
 # Check if subscriptions exist
 if [[ $SUBSCRIPTIONS != "[]" ]]; then
@@ -103,17 +113,15 @@ if [[ $SUBSCRIPTIONS != "[]" ]]; then
     echo $SUBSCRIPTIONS | jq -rc '.[]' | while IFS='' read SUBSCRIPTION; do
         parse_subscription
 
-        # Get role assignments and store in a variable
-        ROLE_ASSIGNMENTS=$(get_subscription_role_assignments "$SUBSCRIPTION_NAME")
-
-        # Process role assignments
-        process_role_assignments "$ROLE_ASSIGNMENTS"
+        # Get and process role assignments
+        get_role_assignments
+        output_debug_info "Role Assignments (JSON): $ROLE_ASSIGNMENTS"
+        parse_role_assignments
 
         output_subscription
     done
 else
-    echo "No subscriptions found"
-    echo $BLANK_LINE
+    output_debug_info "No subscriptions found."
 fi
 
 
